e44ac0bb23441bd4ee4c812402144f6a0a6bbfa2
MykelAllan <michaelallanlomugdang@gmail.com>
2024-06-25 20:04:01 -0400

    added start btn to get category and word

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const Categories = ["Sports", "Superheroes", "Animals"];
+    const Categories = ["Sports", "Superheroes","Animals"];
     const Sports = ["Basketball", "Volleyball", "Soccer"];
     const Superheroes = ["Thor", "Spider Man", "Aquaman"];
-    const Animals = ["Cat", "Zebra", "Anaconda"];
+    const Animals = ["Cat", "Zebra"];
+
+    //for getting the length of the chosen cateory
+    const categMap = {
+        "Sports" : Sports,
+        "Superheroes" : Superheroes,
+        "Animals" : Animals,
+    }
 
     const letters =
         [
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-            'w', 'y', 'x', 'z'
+            'w', 'x', 'y', 'z'
         ]
 
     let guess = "";
     let chosenWord;
+    let chosenTopic;
     let word;
     let spacesInWord;
 
     //getting elements
+    const gameDisplay = document.getElementById('game-display');
+    let startBtn = document.getElementById('start-btn')
     let letter = Array.from(document.querySelectorAll('.letter'));
-    let backspace = document.getElementById('backspace');
     let lblCategory = document.getElementById('category');
     let lblWordToGuess = document.getElementById('guess');
-
 
     letter.forEach((letter, index) => {
         letter.addEventListener('click', () => userAction(letter, index))
     })
 
-    backspace.addEventListener('click', () => {
-        guess = guess.slice(0, -1);
-        console.log((guess));
-        updateGuessWord()
+
+    startBtn.addEventListener('click', () => {
+        console.log('starting game')
+        startGame()
     })
+
+    function startGame() {
+        gameDisplay.classList.remove('disabled')
+
+        chooseACategory();
+        choosingAWord() 
+    }
 
 
     function userAction(letter, index) {
@@ -41,9 +56,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 
+    function chooseACategory() {
+        let ranNum = Math.floor((Math.random() * Categories.length))
+        chosenTopic = Categories[ranNum]
+
+        console.log('chosen topic is' + chosenTopic)
+        
+        //dispaly the category
+        lblCategory.innerText = "Category: " + chosenTopic
+
+
+    }
+
+    function choosingAWord() {
+        let chosenCategArray = categMap[chosenTopic];
+        let ranNum = Math.floor((Math.random() * chosenCategArray.length))
+        chosenWord = chosenCategArray[ranNum]
+
+        console.log("chosen word from " +chosenTopic + ' is '+chosenWord);
+
+        //display the chosen word
+        lblWordToGuess.innerText = chosenWord
+
+
+
+    }
+
     function updateGuessWord() {
         lblWordToGuess.innerText = guess;
-
     }
 
 
